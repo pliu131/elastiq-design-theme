78a48046e411788b0aed6211de61ab72b1812f18
Peter Liu <pliu7@binghamton.edu>
2016-10-26 14:59:50 -0400

    update copy

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -1,10 +1,10 @@
 <section class="page-section page-section--highlight">
   <div class="container">
     <header class="page-section__header">
-      <h1>Interested in working together?</h1>
+      <h1>Let's work together.</h1>
     </header>
 
-    <p class="h2">Let's start laying the groundwork for your project.</p>
+    <p class="h2">Tell us about your project.</p>
 
     <a class="typeform-share link button button--large" href="https://peterliu1.typeform.com/to/pxW6wn" data-mode="1" target="_blank">GET STARTED →</a>
     </a>
